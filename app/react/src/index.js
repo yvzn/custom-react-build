9e1bae64321850600fab7d99891c249a7f72bee7
Yvan Razafindramanana <mail@ludeo.net>
2020-10-07 09:27:48 +0200

    chore(front): Init React front-end

---
+++ app/react/src/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class ClickableButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You clicked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Button'
+    );
+  }
+}
+
+const domContainer = document.querySelector('main');
+ReactDOM.render(e(ClickableButton), domContainer);
